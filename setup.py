9daf61624424727837e0b06e481db8879f7152b5
David Hwang <d.hw4ng@gmail.com>
2015-01-31 02:16:21 -0800

    remove id3strip

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ setup(
     author="David Hwang",
     author_email="d.hw4ng@gmail.com",
     packages=["mp3stuff"],
-    scripts=["bin/id3validate","bin/id3strip","bin/diffmusic","bin/albumart","bin/albumartcrawl"],
+    scripts=["bin/id3validate","bin/diffmusic","bin/albumart","bin/albumartcrawl"],
     description="This does id3 stuff",
     install_requires=[
         "eyeD3 >= 0.7.4",
         "termcolor >= 1.1.0",
-        "Pillow >= 2.0.0"
+        "Pillow >= 2.0.0",
+        "mutagen >= 1.27"
     ],
 )
